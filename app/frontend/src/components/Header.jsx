e08e793c0da5b6321f2aa811bbd175312c8fe992
vinidipaula92 <vinidipaula92@gmail.com>
2022-12-10 18:13:59 -0300

    fix: :bug: Alteração de rota ao clicar no botão "Fale Conosco"

--- app/frontend/src/components/Header.jsx
+++ app/frontend/src/components/Header.jsx
@@ -71,7 +71,7 @@ export default function Header() {
                   </Link>
 
                   <Link
-                    to="/construction"
+                    to="/contact"
                     className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                   >
                     Fale conosco
@@ -199,7 +199,7 @@ export default function Header() {
             </Link>
 
             <Link
-              to="/construction"
+              to="/contact"
               className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
             >
               Fale conosco
